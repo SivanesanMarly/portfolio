e519636c012e5ceb317349f48e896e159e0ada2d
sivanesan <155519706+SivanesanMarly@users.noreply.github.com>
2024-06-14 06:23:07 +0530

    Add files via upload

---
+++ (public)/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".auto-type", {
+    strings : ["Fullstack Developer","Web Designer"],
+    typeSpeed : 50,
+    backSpeed : 150,
+    loop : true
+});
+
+AOS.init({
+    duration: 800,
+    once: false,
+    easing : "linear"
+});
